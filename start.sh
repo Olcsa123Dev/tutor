07cae700dd4f6fc1c98496ce06beb7ed30c54160
OlcsaDev <149895041+Olcsa123Dev@users.noreply.github.com>
2026-03-28 14:14:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,22 +3,9 @@
 export DEBIAN_FRONTEND=noninteractive
 
 PORT=${PORT:-10000}
-python3 -m http.server $PORT &
 
-tmate -S /tmp/tmate.sock new-session -d
-
-echo "Várkozás a tmate linkekre..."
-tmate -S /tmp/tmate.sock wait-for ssh-connection
-
-echo "--- TMATE SESSION ---"
-echo "SSH:"
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
-echo ""
-echo "Web:"
-tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
+echo "--- TERMINAL ---"
+echo "online"
 echo "---------------------------"
 
-while true; do
-  tmate -S /tmp/tmate.sock send-keys "echo 'w: $(date)'" Enter
-  sleep 60
-done
+ttyd -p $PORT bash
