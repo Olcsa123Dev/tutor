abc436e5c6036d5bd40557cb9410a00e3a6ab8b0
OlcsaDev <149895041+Olcsa123Dev@users.noreply.github.com>
2026-03-28 13:53:53 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+tmate -S /tmp/tmate.sock new-session -d
+
+echo "Várkozás a tmate linkekre..."
+tmate -S /tmp/tmate.sock wait-for ssh-connection
+
+echo "--- TMATE SESSION ---"
+echo "SSH:"
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+echo ""
+echo "Web:"
+tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
+echo "---------------------------"
+
+while true; do
+  tmate -S /tmp/tmate.sock send-keys "echo 'w: $(date)'" Enter
+  sleep 60
+done
